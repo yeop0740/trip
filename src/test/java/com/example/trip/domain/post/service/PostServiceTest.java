755b5cbc8c5dfb388dc240b76d34b39a5571b013
yeop0740 <ssucgoo@gmail.com>
2023-09-27 12:43:21 +0900

    [테스트] #4 Post 리스트 조회 테스트 코드 추가

--- src/test/java/com/example/trip/domain/post/service/PostServiceTest.java
+++ src/test/java/com/example/trip/domain/post/service/PostServiceTest.java
@@ -8,6 +8,7 @@ import com.example.trip.domain.post.PostRepository;
 import com.example.trip.domain.post.domain.CreatePostRequest;
 import com.example.trip.domain.post.domain.Post;
 import com.example.trip.domain.post.domain.PostDetailsDto;
+import com.example.trip.domain.post.domain.ReadPostsDto;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -118,7 +120,54 @@ class PostServiceTest {
         }
     }
 
+    @Test
+    public void 게시물_리스트_조회_테스트() throws Exception {
+        // given
+        Category category1 = new Category("category1");
+        Category category2 = new Category("category2");
+        Category category3 = new Category("category3");
 
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        categoryRepository.save(category3);
 
+        CreatePostRequest request1 = CreatePostRequest.builder()
+                .title("test title1")
+                .content("test content1")
+                .locationList(List.of(1L, 2L, 3L, 4L, 5L))
+                .categoryList(List.of(category1.getId(), category2.getId(), category3.getId()))
+                .imageList(List.of(1L, 2L))
+                .tagList(List.of("tag1", "tag2"))
+                .build();
+
+        CreatePostRequest request2 = CreatePostRequest.builder()
+                .title("test title2")
+                .content("test content2")
+                .locationList(List.of())
+                .categoryList(List.of(category1.getId(), category2.getId(), category3.getId()))
+                .imageList(List.of())
+                .tagList(List.of("tag3"))
+                .build();
+
+        Long postId1 = postService.createPost("user", request1);
+        Long postId2 = postService.createPost("user", request2);
+
+        int pageNumber = 0;
+        int pageSize = 10;
+
+        // when
+        ReadPostsDto readPostsDto = postService.readPosts(pageNumber, pageSize);
+        List<Post> posts = postRepository.findAll();
+
+        // then
+        Assertions.assertThat(readPostsDto.getCount()).isEqualTo(posts.size());
+        Assertions.assertThat(readPostsDto.getPageNumber()).isEqualTo(pageNumber);
+        Assertions.assertThat(posts).extracting(
+                        Post::getId,
+                        Post::getTitle)
+                .contains(
+                        Tuple.tuple(posts.get(0).getId(), posts.get(0).getTitle()),
+                        Tuple.tuple(posts.get(1).getId(), posts.get(1).getTitle()));
+    }
 
 }
