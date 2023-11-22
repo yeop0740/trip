264bfeaabf82cc22bb776c41ab0038a9ff2d5da1
jinsoo Kim <jinsoo335@naver.com>
2023-11-22 13:25:42 +0900

    [추가] 마이페이지 API 추가
    - 내 댓글 가져오기
    - 내가 스크랩한 게시물 가져오기
    - 내가 작성한 게시물 가져오기

--- src/main/java/com/example/trip/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/trip/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.example.trip.domain.comment.repository;
 
 import com.example.trip.domain.comment.domain.Comment;
+import com.example.trip.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByMember(@Param("member") Member member);
+
+
 }
