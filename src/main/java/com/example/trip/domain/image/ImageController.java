bd4aa0ad2b832eb54e3a42eb0e320a6ee00aa75a
yeop0740 <ssucgoo@gmail.com>
2023-12-05 03:03:17 +0900

    [추가] #19 스웨거 설정

--- src/main/java/com/example/trip/domain/image/ImageController.java
+++ src/main/java/com/example/trip/domain/image/ImageController.java
@@ -5,6 +5,8 @@ import com.example.trip.domain.image.domain.Image;
 import com.example.trip.domain.member.domain.Member;
 import com.example.trip.global.BaseResponse;
 import com.example.trip.global.annotation.Login;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,12 +22,14 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/image")
+@Tag(name = "Image", description = "이미지 관련 API")
 public class ImageController {
 
     private final ImageManager imageManager;
 
     private final ImageService imageService;
 
+    @Operation(summary = "이미지 등록", description = "1개 이상의 이미지를 S3에 저장한 뒤 데이터베이스에 정보를 저장합니다.")
     @PostMapping
     public BaseResponse<List<Long>> createImage(@Login Member member, @ModelAttribute CreateImageRequest request) throws IOException {
         Map<String, String> imageInfos = imageManager.uploadImages(request.getImages(), UUID.randomUUID());
