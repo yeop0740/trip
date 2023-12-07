18b82caf9c881a52bc632ecd3afb1463fa15ea01
yeop0740 <ssucgoo@gmail.com>
2023-12-07 13:48:14 +0900

    [수정] #23 스웨거 사용을 위한 파라미터 설정 및 리퀘스트 형식 설정 및 불필요한 클래스 제거

--- src/main/java/com/example/trip/domain/image/ImageController.java
+++ src/main/java/com/example/trip/domain/image/ImageController.java
@@ -1,18 +1,20 @@
 package com.example.trip.domain.image;
 
-import com.example.trip.domain.image.domain.CreateImageRequest;
 import com.example.trip.domain.image.domain.Image;
 import com.example.trip.domain.member.domain.Member;
 import com.example.trip.global.BaseResponse;
 import com.example.trip.global.annotation.Login;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,9 +32,9 @@ public class ImageController {
     private final ImageService imageService;
 
     @Operation(summary = "이미지 등록", description = "1개 이상의 이미지를 S3에 저장한 뒤 데이터베이스에 정보를 저장합니다.")
-    @PostMapping
-    public BaseResponse<List<Long>> createImage(@Login Member member, @ModelAttribute CreateImageRequest request) throws IOException {
-        Map<String, String> imageInfos = imageManager.uploadImages(request.getImages(), UUID.randomUUID());
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public BaseResponse<List<Long>> createImage(@Parameter(hidden = true) @Login Member member, @RequestPart MultipartFile[] images) throws IOException {
+        Map<String, String> imageInfos = imageManager.uploadImages(List.of(images), UUID.randomUUID());
         List<Long> imageIds = imageService.createImages(imageInfos);
         return new BaseResponse<>(imageIds);
     }
